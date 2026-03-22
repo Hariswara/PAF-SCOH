b5b2df74787c42f6c1140e98fcf8d45c5236659d
Harish <harrishphotos@gmail.com>
2026-03-22 19:03:36 +0530

    refactor(tests): remove unused UUID import in AuthRepositoryTests

--- backend/src/test/java/com/smartcampus/AuthRepositoryTests.java
+++ backend/src/test/java/com/smartcampus/AuthRepositoryTests.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
